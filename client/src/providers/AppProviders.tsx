90cdea78e2dcec795587bedb132b95c439d4d6a7
stardusty-lab <startdusty.lab@gmail.com>
2026-06-02 17:17:21 +0900

    feature: AppProvider 에 Router 연결
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/providers/AppProviders.tsx
+++ client/src/providers/AppProviders.tsx
@@ -1,9 +1,13 @@
 import type { ReactNode } from "react";
 
+import { BrowserRouter as Router } from "react-router";
+
+import { ENV } from "@/configs/env";
+
 type Props = {
   children?: ReactNode;
 };
 
 export const AppProviders = ({ children }: Props) => {
-  return children;
+  return <Router basename={ENV.BASE_URL}>{children}</Router>;
 };
