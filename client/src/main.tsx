fdb3f25da1e5dd8e9af6aa32eb3e1a34fa9fa4cf
stardusty-lab <startdusty.lab@gmail.com>
2026-06-02 16:04:29 +0900

    chore: msw 설정
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { worker } from "@/mocks/msw/browser";
+
 import App from "./App.tsx";
+
+await worker.start({
+  serviceWorker: {
+    url: `/mockServiceWorker.js`,
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
