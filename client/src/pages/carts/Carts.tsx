561c7b465282d9a356bfa90bfd0456c17668c97b
stardusty-lab <startdusty.lab@gmail.com>
2026-06-04 14:46:01 +0900

    feature: 장바구니 상품 정보 status로 관리해서 화면에 rendering
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/pages/carts/Carts.tsx
+++ client/src/pages/carts/Carts.tsx
@@ -1,32 +1,52 @@
+import { useEffect, useState } from "react";
+
+import { cartsProducts } from "@/mocks/data/carts";
+
+interface CartProduct {
+  id: number;
+  quantity: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+}
+
 export const Carts = () => {
+  const [cartProducts, setCartProducts] = useState<CartProduct[]>([]);
+
+  useEffect(() => {
+    setCartProducts(cartsProducts);
+  }, []);
+
   return (
     <>
       <h1>장바구니</h1>
-      <p>현재 2종류의 상품이 담겨있습니다.</p>
+      <p>현재 {cartProducts.length}종류의 상품이 담겨있습니다.</p>
 
       <input id="all" type="checkbox" />
       <label htmlFor="all">전체선택</label>
 
       <hr />
-      <div>
-        <input id="1" type="checkbox" />
-        <button>삭제</button>
-        <img />
-        <p>상품이름A</p>
-        <p>35000원</p>
-        <button>-</button>2<button>+</button>
-      </div>
-      <hr />
-      <div>
-        <input id="2" type="checkbox" />
-        <button>삭제</button>
-        <img />
-        <p>상품이름B</p>
-        <p>25000원</p>
-        <button>-</button>2<button>+</button>
-      </div>
-
-      <hr />
+      {cartProducts.map((product) => {
+        return (
+          <>
+            <div>
+              <input
+                id={String(product.id)}
+                name={String(product.id)}
+                type="checkbox"
+              />
+              <button>삭제</button>
+              <img src={product.imgUrl} alt="" />
+              <p>{product.name}</p>
+              <p>{product.price}원</p>
+              <button>-</button>
+              {product.quantity}
+              <button>+</button>
+            </div>
+            <hr />
+          </>
+        );
+      })}
 
       <p>총 주문 금액이 100,000원 이상일 경우 무료 배송됩니다.</p>
 
