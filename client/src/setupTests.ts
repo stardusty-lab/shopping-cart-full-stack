1db1be4a789d331d77adb771eae021532ed6b0e3
stardusty-lab <startdusty.lab@gmail.com>
2026-06-04 19:05:43 +0900

    feature: test 에서 msw 를 사용할 수 있도록 설정
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+import { beforeAll, afterEach, afterAll } from "vitest";
+
+import { server } from "./mocks/msw/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
