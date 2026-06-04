81add80b6871a47da187af8e03249d78ccc82c47
stardusty-lab <startdusty.lab@gmail.com>
2026-06-04 14:22:37 +0900

    fix: ENV 상수 변수 에서 .env 환경 변수 들고오기 실패 수정
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/configs/env.ts
+++ client/src/configs/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  BASE_URL: import.meta.env.BASE_URL,
-  API_URL: import.meta.env.API_URL,
+  BASE_URL: import.meta.env.VITE_BASE_URL,
+  API_URL: import.meta.env.VITE_API_URL,
 };
