d8306056f0cf9db3413781e6fcaf05c23478567b
stardusty-lab <startdusty.lab@gmail.com>
2026-06-05 10:06:47 +0900

    feature: Notice 컴포넌트
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

---
+++ client/src/core/components/Notice/Notice.tsx
@@ -0,0 +1,30 @@
+import type { ElementType } from "react";
+
+import { createClassName } from "@/core/utils/classname";
+
+import { View } from "@/core/components/View";
+
+import styles from "./Notice.module.css";
+
+import type { Props } from "./";
+
+const classnameDefault = "ui-notice";
+
+export const Notice = <T extends ElementType>(props: Props<T>) => {
+  const { as = "div", className, children, ...restProps } = props;
+
+  const modifiers = {};
+
+  const classname = createClassName({
+    styles,
+    baseName: classnameDefault,
+    modifiers,
+    className,
+  });
+
+  return (
+    <View as={as} className={classname} {...restProps}>
+      {children}
+    </View>
+  );
+};
