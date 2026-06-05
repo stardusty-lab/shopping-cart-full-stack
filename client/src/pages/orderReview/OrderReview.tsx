52066c9444d3c06178885e2b844a0dc9cfec918f
stardusty-lab <startdusty.lab@gmail.com>
2026-06-05 14:29:21 +0900

    feature: 주문 확인 페이지 디자인 구현
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/pages/orderReview/OrderReview.tsx
+++ client/src/pages/orderReview/OrderReview.tsx
@@ -1,3 +1,24 @@
+import { Layout } from "@/core/components/Layout";
+import { Header } from "@/core/components/Header";
+import { Button } from "@/core/components/Button";
+import { Notice } from "@/core/components/Notice";
+
 export const OrderReview = () => {
-  return null;
+  return (
+    <Layout>
+      <Header leading={<Header.Back onClick={() => {}} />} />
+      <Notice>
+        <p>주문</p>
+        <p>
+          총 2종류의 상품 4개를 주문합니다. <br />
+          최종 결제 금액을 확인해 주세요.
+        </p>
+        <p>총 결제 금액</p>
+        <p>120000원</p>
+      </Notice>
+      <Button variant="primary" size="large" block disabled>
+        결제하기
+      </Button>
+    </Layout>
+  );
 };
