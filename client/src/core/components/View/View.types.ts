ddf92464307f9ab1d9982bf755fdd1eb461cfa9d
stardusty-lab <startdusty.lab@gmail.com>
2026-06-08 16:19:08 +0900

    feature: PolymorphicProps 의 P = {} 를 Record<string, unknown> 으로 변경 - no-empty-object-type 에러 해결
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/core/components/View/View.types.ts
+++ client/src/core/components/View/View.types.ts
@@ -2,7 +2,10 @@ export type Props<T extends React.ElementType = "div"> = {
   as?: T;
 } & React.ComponentPropsWithoutRef<T>;
 
-export type PolymorphicProps<T extends React.ElementType = "div", P = {}> = {
+export type PolymorphicProps<
+  T extends React.ElementType = "div",
+  P = Record<string, unknown>,
+> = {
   as?: T;
 } & Omit<React.ComponentPropsWithoutRef<T>, keyof P | "as"> &
   P;
