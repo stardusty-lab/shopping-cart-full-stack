a1c039da46ced84ade3a3fdefff7f4e383aa1376
stardusty-lab <startdusty.lab@gmail.com>
2026-06-17 19:09:04 +0900

    [FE] feature: 쿠폰 모달에서 coupons 상태값 사용해서 렌더링
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/pages/orderReview/modals/CouponModal.tsx
+++ client/src/pages/orderReview/modals/CouponModal.tsx
@@ -1,47 +1,77 @@
+import { useState } from "react";
+
 import { Modal } from "@/core/components/Modal";
 import { Button } from "@/core/components/Button";
 import { List } from "@/core/components/List";
 import { Checkbox } from "@/core/components/Checkbox";
 
 export const CouponModal = () => {
+  const [coupons] = useState([
+    {
+      id: 1,
+      code: "FIXED5000",
+      name: "5,000원 할인 쿠폰",
+      expirationDate: "2026-11-30",
+      minOrderAmount: 100000,
+    },
+    {
+      id: 2,
+      code: "BOGO",
+      name: "2개 구매 시 1개 무료 쿠폰",
+      expirationDate: "2026-06-30",
+    },
+    {
+      id: 3,
+      code: "FREESHIPPING",
+      name: "5만원 이상 구매 시 무료 배송 쿠폰",
+      expirationDate: "2026-08-31",
+      minOrderAmount: 50000,
+    },
+    {
+      id: 4,
+      code: "MIRACLESALE",
+      name: "미라클모닝 30% 할인 쿠폰",
+      expirationDate: "2026-07-31",
+      validTime: {
+        start: "04:00",
+        end: "07:00",
+      },
+    },
+  ]);
+
   return (
     <Modal>
       <Modal.Header>쿠폰을 선택해 주세요</Modal.Header>
       <p>쿠폰은 최대 2개까지 사용할 수 있습니다.</p>
 
       <List>
-        <List.Item>
-          <List.Item.Box
-            title={<Checkbox label="5,000원 할인 쿠폰" />}
-            description={
-              <>
-                만료일: 2024년 11월 30일 <br />
-                최소 주문 금액: 100,000원
-              </>
-            }
-          ></List.Item.Box>
-        </List.Item>
-        <List.Item>
-          <List.Item.Box>
-            <Checkbox label="2개 구매 시 1개 무료 쿠폰" />
-            만료일: 2024년 11월 30일
-          </List.Item.Box>
-        </List.Item>
-        <List.Item>
-          <List.Item.Box>
-            <Checkbox label="5만원 이상 구매 시 무료 배송 쿠폰" />
-            만료일: 2024년 8월 31일
-            <br />
-            최소 주문 금액: 50,000원
-          </List.Item.Box>
-        </List.Item>
-        <List.Item>
-          <List.Item.Box>
-            <Checkbox label="미라클모닝 30% 할인 쿠폰" />
-            만료일: 2024년 7월 31일 <br />
-            사용 가능 시간: 오전 4시부터 7시까지
-          </List.Item.Box>
-        </List.Item>
+        {coupons.map((coupon) => {
+          return (
+            <List.Item>
+              <List.Item.Box
+                title={<Checkbox label={coupon.name} />}
+                description={
+                  <>
+                    {coupon.expirationDate && (
+                      <>만료일: {coupon.expirationDate}</>
+                    )}
+                    {coupon.minOrderAmount && (
+                      <>
+                        <br /> 최소 주문 금액: {coupon.minOrderAmount}원
+                      </>
+                    )}
+                    {coupon.validTime && (
+                      <>
+                        <br /> 사용 가능 시간: {coupon.validTime.start}부터
+                        {coupon.validTime.end}까지
+                      </>
+                    )}
+                  </>
+                }
+              ></List.Item.Box>
+            </List.Item>
+          );
+        })}
       </List>
 
       <Button variant="primary" size="medium" block>
