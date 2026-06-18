45b5ffeb75bb31fa96696d3becf0ec9989959a8b
stardusty-lab <startdusty.lab@gmail.com>
2026-06-18 17:38:12 +0900

    [BE] test: 30% 할인 쿠폰을 사용하면 할인금액에 반영한다 테스트 작성
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/orderSheets/orderSheets.domain.test.ts
+++ server/src/modules/orderSheets/orderSheets.domain.test.ts
@@ -168,7 +168,27 @@ describe("주문서 금액 요약 정보 계산", () => {
       // Assert
       expect(result).toBe(expectedDiscountCouponAmount);
     });
-    it("30% 할인 쿠폰을 사용하면 할인금액에 반영한다", () => {});
+    it("30% 할인 쿠폰을 사용하면 할인금액에 반영한다", () => {
+      // Arrange
+      const products = [
+        { price: 18000, quantity: 3 },
+        { price: 9900, quantity: 3 },
+      ];
+      const coupons = ["MIRACLESALE"];
+      const shippingFreeBeforeCoupon = 3000;
+
+      const expectedDiscountCouponAmount = 25110;
+
+      // Act
+      const result = calculateCouponDiscountAmount(
+        products,
+        coupons,
+        shippingFreeBeforeCoupon,
+      );
+
+      // Assert
+      expect(result).toBe(expectedDiscountCouponAmount);
+    });
   });
 
   describe("총 결제 금액 계산", () => {
