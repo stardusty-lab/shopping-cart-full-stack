ee6f0d9723a4f69a8e11405e16ef3ff500606883
stardusty-lab <startdusty.lab@gmail.com>
2026-06-18 16:42:56 +0900

    [BE] feature: 최종 배송비 계산 함수 구현
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/orderSheets/orderSheets.domain.ts
+++ server/src/modules/orderSheets/orderSheets.domain.ts
@@ -6,3 +6,24 @@ export const calculateOrderSheetAmount = (
     return acc;
   }, 0);
 };
+
+export const calculateAppliedShippingFee = (
+  orderSheetAmount: number,
+  isRemoteArea: boolean,
+  hasFreeShippingFeeCoupon: boolean,
+  shippingPolicy: {
+    baseShippingFee: number;
+    remoteAreaAdditionalFee: number;
+    freeShippingThreshold: number;
+  },
+): number => {
+  if (orderSheetAmount >= shippingPolicy.freeShippingThreshold) return 0;
+  if (hasFreeShippingFeeCoupon) return 0;
+
+  if (isRemoteArea)
+    return (
+      shippingPolicy.baseShippingFee + shippingPolicy.remoteAreaAdditionalFee
+    );
+
+  return shippingPolicy.baseShippingFee;
+};
