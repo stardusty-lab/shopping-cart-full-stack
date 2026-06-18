7a452188e87698353a932c7a3d81dad659bca38a
stardusty-lab <startdusty.lab@gmail.com>
2026-06-18 18:00:58 +0900

    [BE] feature: 쿠폰 할인 금액 계산 함수 구현 - 개별 쿠폰 할인 금액 계산
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/orderSheets/orderSheets.domain.ts
+++ server/src/modules/orderSheets/orderSheets.domain.ts
@@ -27,3 +27,44 @@ export const calculateAppliedShippingFee = (
 
   return shippingPolicy.baseShippingFee;
 };
+
+export const calculateCouponDiscountAmount = (
+  products: { price: number; quantity: number }[],
+  coupons: string[],
+  shippingFreeBeforeCoupon: number,
+): number => {
+  let couponDiscountAmount = 0;
+
+  coupons.forEach((coupon) => {
+    const getMaxPriceProductPrice = (
+      products: { price: number; quantity: number }[],
+    ): number => {
+      const bogoProducts = products.filter((product) => product.quantity >= 3);
+      const sortedProduct = bogoProducts.sort((a, b) => b.price - a.price);
+
+      const maxPriceProduct = sortedProduct[0];
+
+      return maxPriceProduct.price;
+    };
+    switch (coupon) {
+      case "FIXED5000":
+        couponDiscountAmount += 5000;
+        break;
+      case "BOGO":
+        const maxPriceProduct = getMaxPriceProductPrice(products);
+        couponDiscountAmount += maxPriceProduct;
+        break;
+      case "FREESHIPPING":
+        couponDiscountAmount += shippingFreeBeforeCoupon;
+        break;
+      case "MIRACLESALE":
+        const orderSheetAmount = calculateOrderSheetAmount(products);
+        couponDiscountAmount += orderSheetAmount * 0.3;
+        break;
+      default:
+        break;
+    }
+  });
+
+  return couponDiscountAmount;
+};
