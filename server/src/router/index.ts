4836ce632a8b160e87cfdd75b4cdc2c6fd917653
stardusty-lab <startdusty.lab@gmail.com>
2026-06-19 19:05:33 +0900

    [BE] feature: 배송비 api 구현
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import cartsRouter from "../modules/carts/carts.router.ts";
 import productsRouter from "../modules/products/products.router.ts";
+import shippingFeeRouter from "../modules/shippingFee/shippingFee.router.ts";
 
 const router = Router();
 
 router.use("/carts", cartsRouter);
 router.use("/products", productsRouter);
+router.use("/shipping-fee", shippingFeeRouter);
 
 export default router;
