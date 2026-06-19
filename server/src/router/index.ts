4b67437c3773ad2a30adb2b056f7f6adcd92d505
stardusty-lab <startdusty.lab@gmail.com>
2026-06-19 19:46:16 +0900

    [BE] feature: 쿠폰 api 구현
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import cartsRouter from "../modules/carts/carts.router.ts";
 import productsRouter from "../modules/products/products.router.ts";
 import shippingFeeRouter from "../modules/shippingFee/shippingFee.router.ts";
+import couponsRouter from "../modules/coupons/coupons.router.ts";
 
 const router = Router();
 
 router.use("/carts", cartsRouter);
 router.use("/products", productsRouter);
 router.use("/shipping-fee", shippingFeeRouter);
+router.use("/coupons", couponsRouter);
 
 export default router;
