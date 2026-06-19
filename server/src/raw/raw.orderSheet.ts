264322db780e4a4329e73075d6f7d5c9472ba145
stardusty-lab <startdusty.lab@gmail.com>
2026-06-19 20:41:10 +0900

    [BE] feature: 장바구니, 주문서 DB 에 데이터 추가
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/raw/raw.orderSheet.ts
+++ server/src/raw/raw.orderSheet.ts
@@ -13,11 +13,49 @@ interface RawOrderSheet {
 }
 
 const createOrderSheets = () => {
-  const rawOrderSheets: RawOrderSheet[] = [];
+  const rawOrderSheets: RawOrderSheet[] = [
+    {
+      id: 1,
+      products: [
+        {
+          id: 1,
+          quantity: 2,
+        },
+        {
+          id: 3,
+          quantity: 1,
+        },
+      ],
+      remoteArea: false,
+      coupons: [4],
+    },
+    {
+      id: 2,
+      products: [
+        {
+          id: 2,
+          quantity: 1,
+        },
+      ],
+      remoteArea: false,
+      coupons: [4],
+    },
+    {
+      id: 3,
+      products: [
+        {
+          id: 2,
+          quantity: 4,
+        },
+      ],
+      remoteArea: false,
+      coupons: [2, 4],
+    },
+  ];
   return rawOrderSheets;
 };
 
-export const shippingFeeStore = {
+export const orderSheetStore = {
   orderSheets: createOrderSheets(),
   reset() {
     this.orderSheets = createOrderSheets();
