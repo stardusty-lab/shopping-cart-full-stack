0cf6e6f16055021f4cdb28e8fc814667d0e77012
stardusty-lab <startdusty.lab@gmail.com>
2026-06-22 01:48:26 +0900

    [FE] refactor: 주문서 정보, 금액 요정 정보 요청 데이터 패칭 hook 분리
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/pages/orderReview/useOrderSheet/useOrderSheet.ts
+++ client/src/pages/orderReview/useOrderSheet/useOrderSheet.ts
@@ -1,34 +1,22 @@
 import { useCallback } from "react";
 import { useParams } from "react-router-dom";
 
-import { useLoadData } from "@/services/core/useLoadData";
 import { useExecute } from "@/services/core/useExecute";
 
 import {
-  getOrderSheet,
-  getOrderSheetPricing,
   patchOrderSheetShippingArea,
   patchOrderSheetCoupons,
 } from "@/services/apis/orderSheets/repository";
 
+import { useOrderSheetData } from "./useOrderSheetData";
+import { useOrderSheetPricing } from "./useOrderSheetPricing";
+
 export const useOrderSheet = () => {
   const { id } = useParams<{ id: string }>();
 
-  const orderSheetLoadData = useLoadData({
-    queryFn: useCallback(async () => {
-      if (!id) return;
-
-      return await getOrderSheet({ id: Number(id) });
-    }, [id]),
-  });
-
-  const { status: orderSheetStatus, data: orderSheetData } =
-    orderSheetLoadData.status;
-
-  const totalCount = orderSheetData?.products.reduce((acc, product) => {
-    acc += product.quantity;
-    return acc;
-  }, 0);
+  const orderSheetLoadData = useOrderSheetData();
+  const { products, totalCount, isRemoteArea, couponSelection } =
+    orderSheetLoadData;
 
   const { mutate: updateIsRemoteAreaMutate } = useExecute({
     executeFn: useCallback(
@@ -78,35 +66,21 @@ export const useOrderSheet = () => {
     await updateSelectedCouponsMutate(couponSelection);
   };
 
-  const pricingLoadData = useLoadData({
-    queryFn: useCallback(async () => {
-      if (!id) return;
-
-      return await getOrderSheetPricing({ id: Number(id) });
-    }, [id]),
-  });
-
-  const { data: pricingData } = pricingLoadData.status;
-
-  const paymentAmount = pricingData
-    ? pricingData.orderSheetAmount -
-      pricingData.discountAmount +
-      pricingData.shippingFee
-    : 0;
+  const pricingLoadData = useOrderSheetPricing();
+  const { pricing, paymentAmount } = pricingLoadData;
 
   return {
-    status: orderSheetStatus,
-
-    products: orderSheetData?.products,
+    products,
     totalCount,
 
-    isRemoteArea: orderSheetData?.isRemoteArea,
-    updateIsRemoteArea,
+    isRemoteArea,
 
-    couponSelection: orderSheetData?.selectedCoupons,
-    updateCouponSelection,
+    couponSelection,
 
-    pricing: pricingData,
+    pricing,
     paymentAmount,
+
+    updateIsRemoteArea,
+    updateCouponSelection,
   };
 };
---
+++ client/src/pages/orderReview/useOrderSheet/useOrderSheetPricing.ts
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import { useParams } from "react-router-dom";
+
+import { useLoadData } from "@/services/core/useLoadData";
+
+import { getOrderSheetPricing } from "@/services/apis/orderSheets/repository";
+
+export const useOrderSheetPricing = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const loadData = useLoadData({
+    queryFn: useCallback(async () => {
+      if (!id) return;
+
+      return await getOrderSheetPricing({ id: Number(id) });
+    }, [id]),
+  });
+
+  const { data } = loadData.status;
+  const { refetch } = loadData;
+
+  const paymentAmount = data
+    ? data.orderSheetAmount - data.discountAmount + data.shippingFee
+    : 0;
+
+  return {
+    pricing: data,
+    paymentAmount,
+    refetch,
+  };
+};
