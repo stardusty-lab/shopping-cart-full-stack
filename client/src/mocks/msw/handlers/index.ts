b298340e90135ad966ef902c3c8ca0487f078162
stardusty-lab <startdusty.lab@gmail.com>
2026-06-21 17:24:14 +0900

    [FE] feature: msw handler - orderSheet 생성
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/mocks/msw/handlers/index.ts
+++ client/src/mocks/msw/handlers/index.ts
@@ -3,6 +3,7 @@ import { http, HttpResponse } from "msw";
 import { handlers as cartsHandlers } from "./carts";
 import { handlers as shippingFeeHandlers } from "./shippingFee";
 import { handlers as couponsHandlers } from "./coupons";
+import { handlers as orderSheetsHandlers } from "./orderSheets";
 
 export const handlers = [
   http.get("/health", () => {
@@ -11,4 +12,5 @@ export const handlers = [
   ...cartsHandlers,
   ...shippingFeeHandlers,
   ...couponsHandlers,
+  ...orderSheetsHandlers,
 ];
