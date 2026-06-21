079445b295a36b783bbe1c58df30707906cf9a74
stardusty-lab <startdusty.lab@gmail.com>
2026-06-21 17:03:27 +0900

    [FE] feature: msw handler - shipping-fee 생성
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- client/src/mocks/msw/handlers/index.ts
+++ client/src/mocks/msw/handlers/index.ts
@@ -1,10 +1,12 @@
 import { http, HttpResponse } from "msw";
 
 import { handlers as cartsHandlers } from "./carts";
+import { handlers as shippingFeeHandlers } from "./shippingFee";
 
 export const handlers = [
   http.get("/health", () => {
     return HttpResponse.json({});
   }),
   ...cartsHandlers,
+  ...shippingFeeHandlers,
 ];
